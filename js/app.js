dd85c28f7185388046adfa20b476df7c64353087
Panna <pampukp@gmail.com>
2024-09-04 07:49:22 +0200

    base

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const makeBoxes = () => {
+    let boxList = [];
+    for (let i = 1; i < 16; i++) {
+        boxList.push(`<div id="box-${i}" class="box">${i}</div>`);
+    }
+    return boxList;
+};
+
+const renderBoxes = () => {
+    const boxesContainer = document.querySelector(".boxes");
+    boxesContainer.innerHTML = "";
+    boxesContainer.innerHTML = makeBoxes().join('');
+};
+
+document.addEventListener("DOMContentLoaded", renderBoxes);
