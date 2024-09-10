43a95c39ec00388779154b2c04258a1f1e636366
pancsaa <pampukp@gmail.com>
2024-09-10 13:28:41 +0200

    done

--- js/app.js
+++ js/app.js
@@ -38,15 +38,88 @@ const renderBoxes = () => {
 const boxes=document.querySelectorAll(".box")//ezek a kódsorok hamarabb futnak le mint a callback függvények. ezért itt még nem létezik a renderelt tartalom
 console.log("Boxlista:",boxes); */
 
-document.addEventListener("DOMContentLoaded", () =>{//feterro fuggveny 
-    renderBoxes()
-    const boxes=document.querySelectorAll(".box")
-    console.log("Boxlista:",boxes);
-});
+
 //Tennivalók:
 //1.kivenni az input mező értékét
+const getinputValue=function(){
+    return document.querySelector("#num").value;
+}
+
 //2.megvizsgálni hogy megfelelő-e az érték?(nem üres string,1 és 15 között van)
+const isItCorrect=()=>{
+    const input=getinputValue();
+    if (!input.trim()){
+        return [false, 0];
+    }
+    if (isNaN(input)){
+        return [false,0]
+    }
+    const currentValue=Number(input)
+    if(currentValue<1 || currentValue>15){
+        return [false,0]
+    }
+    return [true, currentValue];
+}
+
 //3.a szinező gomb megragadása és eseményfigyelőt ternni rá
+function sendErrorMessage(){
+    alert("Helytelen értéket adott meg!")
+}
+
+const coloring=()=>{
+    const button=document.querySelector(".card button:nth-child(3)")
+    button.addEventListener("click", ()=>{
+        coloringBox();
+        clearInput();
+    })
+}
+
+const coloringBox=()=>{
+    const boxes=document.querySelectorAll(".box")
+    const [isValid,number]=isItCorrect();
+    if(!isValid){
+        sendErrorMessage();
+        return;
+    }
+    const box=Array.from(boxes).find(box=>Number(box.id)===number)
+    const [r,g,b]=createColor();
+    box.style.backgroundColor=`rgb(${r},${g},${b})`
+}
+
 //4.véletlen szám generálása
+const randomNumber=()=>{
+    return Math.floor(Math.random()*256)
+}
+
 //5. számokból színt készíteni(rgb-3db)
+const createColor=()=>{
+    const r=randomNumber();
+    const g=randomNumber();
+    const b=randomNumber();
+    return [r, g, b];
+}
+
 //6. 3.feladat színező eljárásban alkalmazni ezt a színt.
+document.addEventListener("DOMContentLoaded", () =>{//feterro fuggveny 
+    renderBoxes()
+    coloring()
+    reset(); 
+    /* const boxes=document.querySelectorAll(".box")
+    console.log("Boxlista:",boxes); */
+});
+
+//torles1
+function clearInput(){
+    const inputElement=document.querySelector("#num")
+    inputElement.value="";
+    inputElement.focus();
+}
+
+//torles2(egesz oldal ujrainditasa)
+const reset=()=>{
+    const reetButton=document.querySelector(".card button:nth-child(4)")
+    reetButton.addEventListener("click",()=>{
+        renderBoxes()
+        clearInput()
+    })
+}
